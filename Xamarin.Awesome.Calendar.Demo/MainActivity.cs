953db048d6cfe7969b2495d94b9e95e76f45bae1
Jan Rabe <jan.rabe@exozet.com>
2018-08-08 11:51:17 +0200

    init

---
+++ Xamarin.Awesome.Calendar.Demo/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Com.Archit.Calendardaterangepicker.Customviews;
+
+namespace Xamarin.Awesome.Calendar.Demo
+{
+    [Activity(Label = "Xamarin.Awesome.Calendar.Demo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            DateRangeCalendarView calendarView = FindViewById<DateRangeCalendarView>(Resource.Id.calendar);
+        }
+    }
+}
+
